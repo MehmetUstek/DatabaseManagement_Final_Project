560add990f3d5eefab50ca0fbc74beadd738fe53
Mehmet Üstek <mustek17@ku.edu.tr>
2022-05-06 22:47:19 +0300

    Movies_export.sql

--- data_population/create_actor_FP.sql
+++ data_population/create_actor_FP.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS actor_FP;
 CREATE TABLE actor_FP
-  AS (SELECT person.person_id as AID, person_name as fullName, gender
-      FROM person LEFT OUTER JOIN gender on LEFT OUTER JOIN movie_cast
-      on movie_cast.person_id = person.person_id and movie_cast.gender_id = gender.gender_id
+  AS (SELECT DISTINCT person.person_id as AID, person_name as fullName,  gender
+      FROM person, movie_cast LEFT JOIN gender on movie_cast.gender_id = gender.gender_id
+      where movie_cast.person_id = person.person_id
+      group by person.person_id
       );
 ALTER TABLE actor_FP ADD PRIMARY KEY (AID);
