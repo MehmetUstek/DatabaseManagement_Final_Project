2f762960c8e19689087f7178e167404ff76a3428
Mehmet Üstek <mustek17@ku.edu.tr>
2022-05-27 18:23:15 +0300

    Sign In Page, Home Page

---
+++ flutter/lib/HomePage.dart
@@ -0,0 +1,175 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var containerColor = Colors.black87;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Gradient background = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.topRight,
+    stops: [
+      0.5,
+      0.9,
+    ],
+    colors: [
+      Color(0xffA9C7F2),
+      Color(0xff8DB5EE),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          // color: Colors.white,
+          // margin: const EdgeInsets.only(left: 50.0, right: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Image.asset(
+                    'images/logo.png',
+                    width: 250,
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              Padding(padding: EdgeInsets.only(left:30),
+              child:
+              Row(
+                children: <Widget> [
+              Text(
+                'Welcome, Kerem!',
+                style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 25,
+                    )),
+              ),
+        ]
+              ),
+              ),
+
+              Padding(padding: EdgeInsets.only(top: 40.0)),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          side: BorderSide(
+                              width: 5.0,
+                              color: Colors.black,
+                              style: BorderStyle.solid),
+                        ),
+                        fixedSize: const Size(175, 175),
+                        primary: Colors.white,
+                      ),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              CupertinoIcons.search,
+                              size: 70,
+                              color: Colors.black,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 5),
+                              child: Text(
+                                'Browse',
+                                style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25,
+                                )),
+                              ),
+                            ),
+                          ]),
+                      onPressed: () {},
+                    ),
+                    Padding(padding: EdgeInsets.only(left:20),),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          side: BorderSide(
+                              width: 5.0,
+                              color: Colors.black,
+                              style: BorderStyle.solid),
+                        ),
+                        fixedSize: const Size(175, 175),
+                        primary: Colors.black,
+                      ),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              CupertinoIcons.home,
+                              size: 70,
+                              color: Colors.white,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 5),
+                              child: Text(
+                                'Browse',
+                                style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                )),
+                              ),
+                            ),
+                          ]),
+                      onPressed: () {},
+                    ),
+
+                  ]),
+              Padding(padding:EdgeInsets.only(top:50),child:
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    side: BorderSide(
+                        width: 2.0,
+                        color: Colors.black,
+                        style: BorderStyle.solid),
+                  ),
+                  fixedSize:
+                  Size(MediaQuery.of(context).size.width * 3/7, 52),
+                  primary: Colors.white,
+                ),
+                child: const Text(
+                  'Sign Out',
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
+                onPressed: () {
+                },
+              ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
