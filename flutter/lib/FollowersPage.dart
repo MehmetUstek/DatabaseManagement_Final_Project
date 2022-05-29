636b37097c01b08a64cf5832a090a7c01268fee1
KeremG07 <69321438+KeremG07@users.noreply.github.com>
2022-05-29 19:47:42 +0300

    movie details page added, profile page components added

--- flutter/lib/FollowersPage.dart
+++ flutter/lib/FollowersPage.dart
@@ -1,3 +1,4 @@
+import 'package:database_management_project/OtherProfilePage.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -23,137 +24,122 @@ class _FollowersPageState extends State<FollowersPage> {
     return Scaffold(
       body: Center(
         child: Container(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
+          child: Padding(
+            padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
                       padding: EdgeInsets.only(top: 20),
-                      child: IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: Icon(CupertinoIcons.arrow_left_circle_fill),
-                          iconSize: 36)),
-                  Padding(
-                    padding: EdgeInsets.only(),
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Image.asset('images/logo.png', width: 180),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          elevation: 10,
+                          shape: CircleBorder(),
+                          primary: Colors.white,
+                        ),
+                        child: Icon(
+                          CupertinoIcons.arrowtriangle_left_circle,
+                          size: 30,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(),
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Image.asset('images/logo.png', width: 180),
+                      ),
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                const Padding(
+                  padding: EdgeInsets.only(top: 20),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30, bottom: 30),
+                  child: Row(children: <Widget>[
+                    Text(
+                      'Followers',
+                      style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 40,
+                      )),
+                    ),
+                  ]),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                      side: BorderSide(
+                          width: 1.0,
+                          color: Colors.white,
+                          style: BorderStyle.solid),
+                    ),
+                    elevation: 20.0,
+                    fixedSize: const Size(350, 100),
+                    primary: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 0),
+                                    child: Text(
+                                      'Kerem Girenes',
+                                      style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 25,
+                                      )),
+                                    ),
+                                  ),
+                                ]),
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 5),
+                                    child: Text(
+                                      '@username',
+                                      style: GoogleFonts.montserrat(
+                                          textStyle: TextStyle(
+                                        color: Colors.black45,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                    ),
+                                  ),
+                                ]),
+                          ]),
                     ),
                   ),
-                ],
-              ),
-              const Padding(padding: EdgeInsets.only(top: 10)),
-              const Padding(
-                padding: EdgeInsets.only(top: 20),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 30, bottom:30),
-                child: Row(children: <Widget>[
-                  Text(
-                    'Followers',
-                    style: GoogleFonts.montserrat(
-                        textStyle: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 40,
-                    )),
-                  ),
-                ]),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                    side: BorderSide(
-                        width: 1.0,
-                        color: Colors.white,
-                        style: BorderStyle.solid),
-                  ),
-                  elevation: 20.0,
-                  fixedSize: const Size(350, 100),
-                  primary: Colors.white,
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => OtherProfilePage()));
+                  },
                 ),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 0),
-                                  child: Text(
-                                    'Kerem Girenes',
-                                    style: GoogleFonts.montserrat(
-                                        textStyle: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 25,
-                                    )),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(right: 10),
-                                  child: Text(
-                                    '3',
-                                    style: GoogleFonts.montserrat(
-                                        textStyle: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 25,
-                                            fontWeight: FontWeight.bold)),
-                                  ),
-                                ),
-                              ]),
-                          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 5),
-                              child: Text(
-                                '@username',
-                                style: GoogleFonts.montserrat(
-                                    textStyle: TextStyle(
-                                  color: Colors.black45,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 5),
-                              child: Text(
-                                'watchlists',
-                                style: GoogleFonts.montserrat(
-                                    textStyle: TextStyle(
-                                      color: Colors.black45,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                    )),
-                              ),
-                            ),
-                          ]),
-
-                        ]),
-                  ),
-                ),
-                onPressed: () {},
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: 200, bottom: 10),
-                  child: IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        CupertinoIcons.xmark_circle,
-                        size: 70,
-                      ))),
-            ],
+              ],
+            ),
           ),
         ),
       ),
